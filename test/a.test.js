2da2c8c0756890f268307b01e07957b00f102d7d
Brandon Mathis <brandon@imathis.com>
2018-04-24 13:07:07 -0500

    Found a great headless testing paradigm

--- test/a.test.js
+++ test/a.test.js
@@ -6,9 +6,18 @@ describe('Local', () => {
   it('should display "Submit" text on page', async () => {
     await expect(page).toMatch('Submit')
     await expect(page).toClick('button', { text: 'Submit' })
-    await expect(page).toMatchElement('label.invalid')
-    await expect(page).toFill('#first-input', 'test')
+    await expect(page).toMatchElement('#input-1.invalid')
+
+    await page.focus('#input-1 input')
+    await page.keyboard.type('test', {delay: 100})
+    await expect(page).toMatchElement('#input-1.valid')
+
     await expect(page).toClick('button', { text: 'Submit' })
-    await expect(page).toMatchElement('label.valid')
+    await expect(page).toMatchElement('#input-2.invalid')
+    await expect(page).toMatch('Please enter a valid email address.')
+
+    await page.focus('#input-2 input')
+    await page.keyboard.type('foo@bar.org', {delay: 100})
+    await expect(page).toMatchElement('#input-2.valid')
   })
 })
